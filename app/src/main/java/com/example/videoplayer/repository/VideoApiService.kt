e0b021fb7bb848f89a979530be261929f1247da1
MayankRunija <runija111@gmail.com>
2026-01-18 21:22:00 +0530

    final

--- app/src/main/java/com/example/videoplayer/repository/VideoApiService.kt
+++ app/src/main/java/com/example/videoplayer/repository/VideoApiService.kt
@@ -7,10 +7,12 @@ import retrofit2.http.Query
 interface VideoApiService {
     @GET("youtube/v3/search")
     suspend fun getVideosByCategory(
+        @Query("videoEmbeddable") videoEmbeddable: String = "true",
         @Query("part") part: String = "snippet",
         @Query("maxResults") maxResults: Int = 20,
         @Query("q") query: String,
         @Query("type") type: String = "video",
-        @Query("key") apiKey: String = "AIzaSyBcxjUTJnH5wMmZrLtczza_TsRmlSqddEQ" // Put your key here
+        @Query("key") apiKey: String = "AIzaSyBcxjUTJnH5wMmZrLtczza_TsRmlSqddEQ",
+
     ): YouTubeResponse
 }
